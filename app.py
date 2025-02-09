6c5c5152b7f2c92ae1204ddf88198e1e6eac7d39
kopalg20 <kopalgarg2005@gmail.com>
2025-02-10 00:10:18 +0530

    Added port

--- app.py
+++ app.py
@@ -79,5 +79,6 @@ def index():
 
     return render_template('questionarrie.html')
 
-if __name__=="__main__":
-    app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port, debug=True)
